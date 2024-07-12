9c30d888da52f85ec9b6edc6132d67b8a306c35b
garec99054 <garec99054@gmail.com>
2024-07-12 10:44:06 -0400

    Configure for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const { i18n } = require('./next-i18next.config.js');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
